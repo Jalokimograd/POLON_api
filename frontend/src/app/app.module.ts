003c5eb3fe5ec015b0f594388f874dc38b86abf5
Dram <twiwala@dram.pl>
2021-01-06 00:36:57 +0100

    init packages & header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HttpClientModule } from '@angular/common/http';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { CoreModule } from './core/core.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    CoreModule,
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    NgxSpinnerModule,
+    MatSnackBarModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: window?.navigator?.language || 'en-US'},
+    {provide: MAT_MOMENT_DATE_ADAPTER_OPTIONS, useValue: {useUtc: false}},
+    {provide: MAT_DATE_LOCALE, useValue: window?.navigator?.language || 'en-US'},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ frontend/src/app/core/core-routing.module.ts
@@ -0,0 +1,27 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [
+  {
+    path: 'patent',
+    loadChildren: () => import('./patent/patent.module').then(mod => mod.PatentModule),
+  },
+  {
+    path: 'publication',
+    loadChildren: () => import('./publication/publication.module').then(mod => mod.PublicationModule),
+  },
+  {
+    path: 'comparison',
+    loadChildren: () => import('./comparison/comparison.module').then(mod => mod.ComparisonModule),
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class CoreRoutingModule {
+
+}
